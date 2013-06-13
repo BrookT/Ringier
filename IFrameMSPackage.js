36177610447b296c7c9d2b543e2f501e9bc978e9
Brook Tang <787305552@qq.com>
2013-06-13 18:00:14 +0800

    Update IFrameMSPackage.js
    publish version

--- IFrameMSPackage.js
+++ IFrameMSPackage.js
@@ -36,7 +36,7 @@ var iframeRequest =
         Rg.net.ajax.Post.Ajax(postData, "createIframe", iframeCallBack.createIFrame, iframeCallBack.error);
     },
     updateIFrame: function (postData) {
-        Rg.net.ajax.Post.Ajax(postData, "updateIframe", iframeCallBack.updateIframe,iframeCallBack.error);
+        Rg.net.ajax.Post.Ajax(postData, "updateIframe", iframeCallBack.updateIFrame, iframeCallBack.error);
     },
     deleteIFrame: function (postData)
     {
@@ -50,22 +50,43 @@ var iframeCallBack = {
         result = result.d;
         if (result.Flag)
         {
+            if (result.ReturnData.Data.ID != '0') {
+                status = 'update';
+            }
+            else { status = 'insert'; }
+            $("#iframeLink").val(result.ReturnData.Data.IFrameLink);
+            $("#iframeHeight").val(result.ReturnData.Data.IFrameHeight);
+            $("#iframeWidth").val(result.ReturnData.Data.IFrameWidth);
+            $("#iframeIsDisplay").val(result.ReturnData.Data.IsDisplay == true ? 1 : 0);
+            currentID = result.ReturnData.Data.ID;
+            pageLanguage = result.ReturnData.Data.PageLanguage;
+            siteID = result.ReturnData.Data.SiteID;
+            positionInSite = result.ReturnData.Data.PositionInSite;
             //返回数据
         }
     },
     createIFrame: function (result) {
         result = result.d;
-        if (result.Flag)
-        {
+        if (result.Flag) {
+            currentID = result.ReturnData.Data;
+            if (currentID != '0')
+            { status = 'update'; }
+            else
+            {
+                status = 'insert';
+            }
+            Rg.message.growlUI(result.Message);
             //创建后提示操作成功
         }
+        else { Rg.message.growlUI(result.Message); }
     },
     updateIFrame: function (result) {
         result = result.d;
-        if (result.Flag)
-        {
-           //更新成功逻辑
+        if (result.Flag) {
+            Rg.message.growlUI(result.Message);
+            //更新成功逻辑
         }
+        else { Rg.message.growlUI(result.Message); }
     },
     deleteIFrame: function (result) {
         if (result.Flag)
