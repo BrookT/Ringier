aeb329c772616aaaca4a88eac639f24a84a7f1ed
Brook Tang <787305552@qq.com>
2013-06-28 11:27:55 +0800

    Update sqlTool.cs
    still need extension for models,bll,dal,webservice,javascript

--- sqlTool.cs
+++ sqlTool.cs
@@ -1,4 +1,4 @@
-//Brook 2013-6-27
+//Brook 2013-6-28 11:26
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,19 +53,23 @@ namespace workTools
                     ColumnID=6,
                     ColumnName="CreateDate",
                     ColumnType=ColumnType.DATETIME.ToString(),
-                    DefaultValue="GETDATE()"
+                    DefaultValue="GETDATE()",
+                    IsCreateTime=true
                 },
                 new DataModel(){
                     ColumnID=7,
                     ColumnName="UpdateDate",
                     ColumnType=ColumnType.DATETIME.ToString(),
-                    DefaultValue="GETDATE()"
+                    DefaultValue="GETDATE()",
+                    IsUpdateTime=true
                 }
             };
+            Console.WriteLine("Creating ...");
             tsql.output(tsql.CreateTable(ColumnList), TSQL.outputPath, TSQL.dir, TSQL.tblName, ".sql");
-            string procResult=TSQL.createTSQLHeader()+tsql.CreateProc(ColumnList,"create")+tsql.CreateProc(ColumnList,"delete")+tsql.CreateProc(ColumnList,"update")+tsql.CreateProc(ColumnList,"get");
+            string procResult = TSQL.createTSQLHeader() + tsql.CreateProc(ColumnList, "create") + tsql.CreateProc(ColumnList, "delete") + tsql.CreateProc(ColumnList, "update") + tsql.CreateProc(ColumnList, "get") + tsql.CreateProc(ColumnList, "getList");
             tsql.output(procResult ,TSQL.outputPath,TSQL.dir,TSQL.procName,".sql");
-            Console.Write(tsql.CreateTable(ColumnList));
+
+            Console.Write("complete!");
             Console.ReadKey();
         }
     }
@@ -89,6 +93,10 @@ namespace workTools
         public bool IsAutoGrow { get; set; }
 
         public string DefaultValue { get; set; }
+
+        public bool IsUpdateTime { get; set; }
+
+        public bool IsCreateTime { get; set; }
     }
 
     public class ColumnType
@@ -115,6 +123,7 @@ namespace workTools
         public static string procRName = "Get" + metaName;
         public static string procUName = "Update" + metaName;
         public static string procDName = "Delete" + metaName;
+        public static string procGLName = "Get" + metaName + "List";
         public static string devName = "Brook";
         IList<DataModel> ColumnList;
         public TSQL()
@@ -219,10 +228,145 @@ namespace workTools
                     result += ");\t";
                     result += "SET @sys_id=@@IDENTITY;\t";
                     result += "END\t";
-                    result += "GO\t";
+                    result += "GO\t\t";
                     break;
-                case "get": break;
-                case "update": break;
+                case "get":
+                    result += "CREATE PROC "+procRName+"\t";
+                    foreach (DataModel column in ColumnList)
+                    {
+                        if (column.IsPrimaryKey)
+                        {
+                            result += "@"+column.ColumnName+" "+column.ColumnType+"\t";
+                        }
+                    }
+                    result += "AS\t";
+                    result += "BEGIN\t";
+                    result += "SELECT * FROM [" + tblName + "] WHERE ";
+                    foreach (DataModel column in ColumnList)
+                    {
+                        if (column.IsPrimaryKey)
+                        {
+                            result += "@" + column.ColumnName + "=" + column.ColumnName + ";\t";
+                        }
+                    }
+                    result += "END\t";
+                    result += "GO\t";
+                    ; break;
+                case "getList":
+                    result += "CREATE PROC "+procGLName+"\t";
+                    List<DataModel> ColumnTemp = new List<DataModel>();
+                    foreach (DataModel column in ColumnList)
+                    {
+                        if (!column.IsPrimaryKey && !column.IsCreateTime && !column.IsUpdateTime)
+                        {
+                            ColumnTemp.Add(column);
+                        }
+                    }
+                    columnCount = 0;
+                    foreach (DataModel column in ColumnTemp)
+                    {
+                            result += "@"+column.ColumnName+" "+column.ColumnType+" ,\t";
+                    }
+                    result += "@StartRowID int,\t";
+                    result += "@EndRowID int \t";
+                    result += "AS\t";
+                    result += "BEGIN\t";
+                    result += "DECLARE @SQL NVARCHAR(4000)='';\t";
+                    result += "DECLARE @WHERE NVARCHAR(4000)='';\t";
+                    result += "DECLARE @ORDER NVARCHAR(4000)='';\t";
+                    result += "SET @SQL=@SQL+'SELECT * from ["+tblName+"]' \t";
+                    result += "SET @WHERE=@WHERE+'WHERE 1=1 ' \t";
+                    result+="SET @ORDER=@ORDER+'ORDER BY \t";
+                    foreach (DataModel column in ColumnList)
+                    {
+                        if (column.IsPrimaryKey)
+                        {
+                            result += "["+column.ColumnName+"] desc,";
+                        }
+                        if (column.IsCreateTime)
+                        {
+                            result += "["+column.ColumnName+"] desc,";
+                        }
+                        if (column.IsUpdateTime)
+                        { 
+                            result+="["+column.ColumnName+"] desc'\t";
+                        }
+                    }
+                    foreach (DataModel column in ColumnList)
+                    {
+                        string obj_temp;
+                        if (!column.IsPrimaryKey && !column.IsUpdateTime && !column.IsCreateTime)
+                        {
+                            if (IsTypeStringable(column.ColumnType))
+                            {
+                                obj_temp = "@" + column.ColumnName + "";
+                            }
+                            else
+                            {
+                                obj_temp = "CONVERT(NVARCHAR(20),@"+column.ColumnName+")";
+                            }
+                            result += "IF(LTRIM(RTRIM(ISNULL("+obj_temp+",''))))<>''\t";
+                            result += "BEGIN\t";
+                            if (IsTypeStringable(column.ColumnType))
+                            {
+                                result += "SET @WHERE=@WHERE+' AND [" + column.ColumnName + "]='''+" + obj_temp + "+''' ';\t";
+                            }
+                            else {
+                                result += "SET @WHERE=@WHERE+' AND [" + column.ColumnName + "]='+" + obj_temp + "+ ' ';\t";
+                            }
+                            result += "END\t";
+                        }
+                    }
+                    result += "SET @SQL=@SQL+@WHERE+@ORDER;\t";
+                    result += "EXEC (@SQL);\t";
+                    result += "END\t";
+                    result += "GO\t\t";
+                    break;
+                case "update":
+                    result += "CREATE PROC "+procUName+" \t";
+                    columnCount=0;
+                    foreach(DataModel column in ColumnList)
+                    {
+                        columnCount++;
+                        result += "@"+column.ColumnName+" "+column.ColumnType+"";
+                        if (columnCount == ColumnList.Count)
+                        {
+                            result += ",";
+                        }
+                        result += "\t";
+                    }
+                    result += "AS\t";
+                    result += "BEGIN\t";
+                    result += "UPDATE ["+tblName+"] SET \t";
+                    columnCount = 0;
+                    foreach (DataModel column in ColumnList)
+                    { 
+                        columnCount++;
+                        if (!column.IsPrimaryKey&&!column.IsUpdateTime)
+                        {
+                            result += "["+column.ColumnName+"]=@"+column.ColumnName+"";
+                        }
+                        else if (column.IsUpdateTime)
+                        {
+                            result += "[" + column.ColumnName + "]=" + column.DefaultValue + "" ;
+                        }
+                        if (columnCount != ColumnList.Count&&!column.IsPrimaryKey)
+                        {
+                            result += ",";
+                        }
+                        result += "\t";
+                    }
+                    result+=" WHERE \t";
+                    foreach (DataModel column in ColumnList)
+                    {
+                        if (column.IsPrimaryKey)
+                        {
+                            result += "@"+column.ColumnName+"="+column.ColumnName+"\t";
+                        }
+                    }
+                    result += "END\t";
+                    result += "GO\t\t";
+                    break;
                 case "delete":
                     result += "CREATE PROC " + procDName + " \t";
                     foreach (DataModel column in ColumnList)
@@ -243,8 +387,26 @@ namespace workTools
                         }
                     }
                     result += "END\t";
-                    result += "GO\t";
+                    result += "GO\t\t";
                     ; break;
+            }
+            return result;
+        }
+
+        public static bool IsTypeStringable(string type)
+        {
+            bool result=false;
+            switch (type)
+            { 
+                case "int":
+                case "bit":
+                case "DateTime": result = false; break;
+                case "NTEXT":
+                case "NVARCHAR(20)":
+                case "NVARCHAR(8)":
+                case "NVARCHAR(255)":
+                case "NVARCHAR(max)": result = true; break;
+                default: break;
             }
             return result;
         }
